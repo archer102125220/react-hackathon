a99aa195158ca3861b5eb204374ef1d8dd1513ef
archer <archer102125220.2015@gmail.com>
2019-04-26 19:37:54 +0800

    Updates

---
+++ precache-manifest.136a0c8293daa974481a2d149ca78262.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b74ae2944cb5b775b4efe1fbcf70bdc3",
+    "url": "/react-hackathon/static/media/stadistics.b74ae294.png"
+  },
+  {
+    "revision": "ff05dc4dad240614d98afb8fe10733dc",
+    "url": "/react-hackathon/static/media/map2.ff05dc4d.png"
+  },
+  {
+    "revision": "b2d3c123335c5182ff2f059166adef17",
+    "url": "/react-hackathon/static/media/house.b2d3c123.png"
+  },
+  {
+    "revision": "eccffa0769d86172c771fba6318d8bc4",
+    "url": "/react-hackathon/static/media/commerce-and-shopping.eccffa07.png"
+  },
+  {
+    "revision": "432693c3420bfc729df8",
+    "url": "/react-hackathon/static/js/runtime~main.05d2b06c.js"
+  },
+  {
+    "revision": "4ea3bac3517f1a203c78",
+    "url": "/react-hackathon/static/js/main.1e0d7920.chunk.js"
+  },
+  {
+    "revision": "7d6aedc2b89d39d2130c",
+    "url": "/react-hackathon/static/js/2.598f176e.chunk.js"
+  },
+  {
+    "revision": "4ea3bac3517f1a203c78",
+    "url": "/react-hackathon/static/css/main.38cf95b8.chunk.css"
+  },
+  {
+    "revision": "7d6aedc2b89d39d2130c",
+    "url": "/react-hackathon/static/css/2.82ee8de2.chunk.css"
+  },
+  {
+    "revision": "fd6eaa5739113dcc3c3fe96015a7352f",
+    "url": "/react-hackathon/index.html"
+  }
+];
