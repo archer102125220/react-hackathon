2fa559d86493ecbdbb0ebb97ee8656d2c85bb428
archer <archer102125220.2015@gmail.com>
2019-04-27 20:41:58 +0800

    萬國語系套件bulid完成

---
+++ build/precache-manifest.660b7606a46ca94fc84ad4b5350f2b9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b74ae2944cb5b775b4efe1fbcf70bdc3",
+    "url": "/react-hackathon/static/media/stadistics.b74ae294.png"
+  },
+  {
+    "revision": "ff05dc4dad240614d98afb8fe10733dc",
+    "url": "/react-hackathon/static/media/map2.ff05dc4d.png"
+  },
+  {
+    "revision": "b2d3c123335c5182ff2f059166adef17",
+    "url": "/react-hackathon/static/media/house.b2d3c123.png"
+  },
+  {
+    "revision": "eccffa0769d86172c771fba6318d8bc4",
+    "url": "/react-hackathon/static/media/commerce-and-shopping.eccffa07.png"
+  },
+  {
+    "revision": "432693c3420bfc729df8",
+    "url": "/react-hackathon/static/js/runtime~main.05d2b06c.js"
+  },
+  {
+    "revision": "b7d2737a30f7d1504321",
+    "url": "/react-hackathon/static/js/main.99b723f7.chunk.js"
+  },
+  {
+    "revision": "3a925420098fdd6fc25a",
+    "url": "/react-hackathon/static/js/2.a7796dbd.chunk.js"
+  },
+  {
+    "revision": "b7d2737a30f7d1504321",
+    "url": "/react-hackathon/static/css/main.38cf95b8.chunk.css"
+  },
+  {
+    "revision": "3a925420098fdd6fc25a",
+    "url": "/react-hackathon/static/css/2.82ee8de2.chunk.css"
+  },
+  {
+    "revision": "a5d3712aa5c6bd0936054f1e983d22b0",
+    "url": "/react-hackathon/index.html"
+  }
+];
